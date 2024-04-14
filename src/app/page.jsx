3859079fdf72d5a2a2c47a0e2fe97ccb83b46945
Sonia Moreno <94900792+Soniaa1995@users.noreply.github.com>
2024-04-14 10:32:38 +0200

    titulo pagina y navbar basico-sin-responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,17 @@
 export default function Page() {
   return (
-    <nav>
-      <h1>Hola guapos</h1>
+    <nav className="bg-black">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-5 ">
+        <span className="text-white font-semibold">La vaca dice Muu</span>
+        <div className="text-amber-500 font-semibold text-center ">
+          <ul className="inline-flex ">
+            <li className="mr-10">Inicio</li>
+            <li className="mr-10">Carta</li>
+            <li className="mr-10">Promociones</li>
+            <li className="mr-10">Contacta con nosotros</li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }

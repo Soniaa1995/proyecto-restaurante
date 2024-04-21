58ae0c0a829e1ca089f1409f57fe7eec49362766
Sonia Moreno <94900792+Soniaa1995@users.noreply.github.com>
2024-04-21 11:48:15 +0200

    main terminado a falta de los detalles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,16 @@
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
 
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
 
 export default function Page() {
   return (
     <div className="flex min-h-screen flex-col bg-black text-white">
       <Navbar />
-      <main className="max-w-2xl mx-auto grow space-y-8"></main>
+      <Main />
       <Footer />
-
     </div>
   );
 }
+
+//

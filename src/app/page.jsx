31813d9f7d2ba400a38b7c24c5c2af640f557801
Sonia Moreno <94900792+Soniaa1995@users.noreply.github.com>
2024-10-30 11:02:56 +0100

    cambio tamaño texto para resolucion 2k

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,7 +13,7 @@ export default function Page() {
           alt="img"
           className="block w-full h-auto sm:h-none sm:max-w-none"
         />
-        <div className="absolute top-[90%] left-2/4 translate-x-[-50%] translate-y-[-50%] text-white font-semibold sm:top-[85%] text-xs sm:text-sm md:text-2xl lg:text-[1.7rem] font-ubuntoSans">
+        <div className="absolute top-[90%] left-2/4 translate-x-[-50%] translate-y-[-50%] text-white font-semibold sm:top-[85%] text-xs sm:text-sm md:text-2xl lg:text-[1.7rem] xl:text-[2rem] font-ubuntoSans">
           <p>Las mejores hamburguesas</p>
           <p>Solo para los más atrevidos</p>
         </div>
@@ -24,7 +24,7 @@ export default function Page() {
           alt="img"
           className="block w-full h-auto sm:h-none sm:max-w-none"
         />
-        <div className="absolute top-[30%] left-[60%] translate-x-[-30%] translate-y-[-50%] text-white font-semibold sm:top-[20%] text-xs sm:text-sm md:text-2xl lg:text-[1.7rem] text-center font-ubuntoSans">
+        <div className="absolute top-[30%] left-[60%] translate-x-[-30%] translate-y-[-50%] text-white font-semibold sm:top-[20%] text-xs sm:text-sm md:text-2xl lg:text-[1.7rem] xl:text-[2rem] text-center font-ubuntoSans">
           <h1 className="">¿Te atreves a probar nuestra carta?</h1>
           <button className="border w-24 h-5 mt-3 sm:w-48 sm:h-12 rounded-md sm:mt-8">
             <Link href="/menu">Ver carta</Link>
@@ -38,7 +38,7 @@ export default function Page() {
           alt="img"
           className="block w-full h-auto sm:h-none sm:max-w-none"
         />
-        <div className="absolute top-[20%] left-[60%] translate-x-[-30%] translate-y-[-50%] text-white font-semibold sm:top-[20%] text-xs sm:text-sm md:text-2xl lg:text-[1.7rem] text-center font-ubuntoSans">
+        <div className="absolute top-[20%] left-[60%] translate-x-[-30%] translate-y-[-50%] text-white font-semibold sm:top-[20%] text-xs sm:text-sm md:text-2xl lg:text-[1.7rem] xl:text-[2rem] text-center font-ubuntoSans">
           <h1>Conoce todas nuestras promociones</h1>
           <button className="border w-24 h-6 mt-2 sm:w-48 sm:h-12 rounded-md sm:mt-8">
             <Link href="/promociones">Promociones</Link>
